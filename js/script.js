2afa68c1c0ebb0ac1bbdeafb684efd71c4e11648
Serhii Morykon <sergomen153@gmail.com>
2017-02-23 02:32:42 +0200

    improve map script and edit hover effects on the catalog item

--- js/script.js
+++ js/script.js
@@ -52,3 +52,27 @@ var buttonWrite=document.querySelector(".map-btn");
     			
     	   		}
     	});
+
+
+        function initialize() {
+    var x = 45.038739;
+    var y = 38.973099;
+    var mapOptions = {
+        zoom: 17,
+        center: new google.maps.LatLng(x, y),
+        scrollwheel: false,
+        disableDefaultUI: true
+    }
+    var map = new  google.maps.Map(
+        document.querySelector(".map-script"),
+        mapOptions
+    );
+    var image = "img/map-marker.png"; 
+    var myLatLng = new google.maps.LatLng(x, y);
+    var beachMarker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        icon: image
+    });
+}
+google.maps.event.addDomListener(window, "load", initialize);
